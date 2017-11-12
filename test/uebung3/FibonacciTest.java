c48f3b9c3b1451258b4d45ede4e9fc4645b15d32
Andreas Reitz <a.reitz@fs.de>
2017-11-12 15:00:52 +0100

    added 2nd Test

--- test/uebung3/FibonacciTest.java
+++ test/uebung3/FibonacciTest.java
@@ -8,12 +8,25 @@ import uebung3.Fibonacci;
 
 public class FibonacciTest {
 	
+	
+	
 	@Test
-	public void testFibonacci(){
+	public void testFibonacci0(){
 		Fibonacci f = new Fibonacci();
 		int n = 0;
 		int[] b = new int[n];
 		b = f.calcFibonacci(n);
 		assertEquals("0 is ok", b[0], 0);
 	}
+	
+	@Test
+	public void testFibonacci9(){
+		Fibonacci f = new Fibonacci();
+		int n = 9;
+		int[] b = new int[n];
+		int[] result = {0,1,1,2,3,5,8,13,21,34};
+		b = f.calcFibonacci(n);
+		Assert.assertArrayEquals(("0 is ok", b, result);
+	}
+	
 }
